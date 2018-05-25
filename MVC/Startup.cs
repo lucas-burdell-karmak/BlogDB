f264cf3101ffa186f731c10ef4748ab27996c60d
lucas-burdell <lburdell@karmak.com>
2018-05-25 14:31:54 -0500

    Fix MVC to be compatible with core changes

--- MVC/Startup.cs
+++ MVC/Startup.cs
@@ -22,7 +22,7 @@ namespace The_Intern_MVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<BlogDB.Core.IBusinessLogic, BlogDB.Core.BusinessLogic>();
+            services.AddSingleton<BlogDB.Core.PostDataAccess>();
             services.AddMvc();
         }
 
