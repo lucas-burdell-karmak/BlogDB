f2484f8d9543c77769b00d96f82a91ad0df1afcb
Lucas Burdell <LBurdell@karmak.com>
2018-05-29 11:41:18 -0500

    [WIP] add some tests

---
+++ BlogCore.Tests/PostTests.cs
@@ -0,0 +1,53 @@
+using System;
+using Xunit;
+using BlogDB.Core;
+
+namespace BlogCore.Tests
+{
+    public class Post
+    {
+        [Fact]
+        public void TestAuthor()
+        {
+            var post = new BlogDB.Core.Post("titleName", "authorName", "bodyText", DateTime.UtcNow, Guid.NewGuid());
+
+            Assert.Equal(post.Author, "authorName");
+        }
+
+        [Fact]
+        public void TestTitle()
+        {
+            var post = new BlogDB.Core.Post("titleName", "authorName", "bodyText", DateTime.UtcNow, Guid.NewGuid());
+
+            Assert.Equal("titleName", post.Title);
+        }
+
+        [Fact]
+        public void TestBody()
+        {
+            var post = new BlogDB.Core.Post("titleName", "authorName", "bodyText", DateTime.UtcNow, Guid.NewGuid());
+
+            Assert.Equal("bodyText", post.Body);
+        }
+
+        [Fact]
+        public void testDateTime()
+        {
+            var date = DateTime.UtcNow;
+            
+            var post = new BlogDB.Core.Post("titleName", "authorName", "bodyText", date, Guid.NewGuid());
+
+            Assert.Equal(date, post.Timestamp);
+        }
+
+        [Fact]
+        public void testPostID()
+        {
+            var guid = Guid.NewGuid();
+
+            var post = new BlogDB.Core.Post("titleName", "authorName", "bodyText", DateTime.UtcNow, guid);
+
+            Assert.Equal(guid, post.PostID);
+        }
+    }
+}
