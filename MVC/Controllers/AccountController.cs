4044da2279f93da77da1155cb66f7e131d9f1252
lucas-burdell <lburdell@karmak.com>
2018-06-07 08:48:24 -0500

    move profile page to Account folder

--- MVC/Controllers/AccountController.cs
+++ MVC/Controllers/AccountController.cs
@@ -17,7 +17,23 @@ namespace The_Intern_MVC.Controllers
     public class AccountController : ControllerBase
     {
 
+        [AllowAnonymous]
+        [HttpGet]
         public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult DeleteProfile()
         {
             return View();
         }
