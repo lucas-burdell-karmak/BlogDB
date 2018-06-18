f77a3ced8c4f11aa0178cd29ef1924a4e6a78ec3
CalvinVest <40300810+CalvinVest@users.noreply.github.com>
2018-06-18 15:29:07 -0500

    Update SearchCriteria.cs

--- MVC/Models/SearchCriteria.cs
+++ MVC/Models/SearchCriteria.cs
@@ -1,21 +1,13 @@
-using System;
 using System.ComponentModel.DataAnnotations;
-
 
 namespace The_Intern_MVC.Models
 {
     public class SearchCriteria
     {
-
         [Required]
         [StringLength(30, MinimumLength = 3)]
         public string SearchString { get; set; }
 
-        public SearchCriteria()
-        {
-            SearchString = "";
-        }
-
-
+        public SearchCriteria() => SearchString = "";
     }
 }
