0bfa45970e8192038617ad3dd217635ca8e72a78
CalvinVest <40300810+CalvinVest@users.noreply.github.com>
2018-06-18 15:20:45 -0500

    Update PostDataAccess.cs

--- BlogCore/Core/PostDataAccess.cs
+++ BlogCore/Core/PostDataAccess.cs
@@ -55,36 +55,23 @@ namespace BlogDB.Core
         public List<Author> GetAllAuthors() => _authorRepo.GetListOfAuthors();
         public List<Post> GetAllPosts() => _postRepo.GetAllPosts();
 
-        public List<int> GetListOfAuthorIDs()
-        {
-            var authors = _authorRepo.GetListOfAuthors();
-            return authors.Select(x => x.ID).ToList();
-        }
+        public List<int> GetListOfAuthorIDs() => _authorRepo.GetListOfAuthors().Select(x => x.ID).ToList();
 
-        public List<string> GetListOfAuthorNames()
-        {
-            var authors = _authorRepo.GetListOfAuthors();
-            return authors.Select(x => x.Name).ToList();
-        }
+        public List<string> GetListOfAuthorNames() => _authorRepo.GetListOfAuthors().Select(x => x.Name).ToList();
 
         public List<Post> GetListOfPostsByAuthorID(int authorID)
         {
             var listOfPostsByAuthor = _postRepo.GetAllPostsByAuthor(authorID);
-            return (listOfPostsByAuthor == null) ? new List<Post>() : listOfPostsByAuthor;
+            if (listOfPostsByAuthor == null)
+                return new List<Post>();
+            return listOfPostsByAuthor;
         }
 
-        public Post GetPostById(Guid id)
-        {
-            var listOfPosts = _postRepo.GetAllPosts();
-            return listOfPosts.FirstOrDefault(x => x.PostID == id);
-        }
+        public Post GetPostById(Guid id) => _postRepo.GetAllPosts().FirstOrDefault(x => x.PostID == id);
 
         public int GetPostCount() => _postRepo.GetAllPosts().Count;
 
-        public Post GetPostFromList(List<Post> listOfPosts, Guid id)
-        {
-            return listOfPosts.FirstOrDefault(x => x.PostID == id);
-        }
+        public Post GetPostFromList(List<Post> listOfPosts, Guid id) => listOfPosts.FirstOrDefault(x => x.PostID == id);
 
         public List<Post> GetSortedListOfPosts(PostComponent sortType)
         {
@@ -102,16 +89,6 @@ namespace BlogDB.Core
             }
         }
 
-        public List<Post> SearchBy(Func<Post, bool> filter)
-        {
-            var posts = _postRepo.GetAllPosts();
-            var results = new List<Post>();
-            posts.ForEach((post) =>
-            {
-                if (filter(post))
-                    results.Add(post);
-            });
-            return results;
-        }
+        public List<Post> SearchBy(Func<Post, bool> filter) => _postRepo.GetAllPosts().Where(x => filter(x)).ToList();
     }
 }
