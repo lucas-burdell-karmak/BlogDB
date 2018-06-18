d8c4d25db11954d482d094aa026493e50f137e52
CalvinVest <40300810+CalvinVest@users.noreply.github.com>
2018-06-18 15:23:11 -0500

    Update Program.cs

--- MVC/Program.cs
+++ MVC/Program.cs
@@ -1,18 +1,15 @@
-﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace The_Intern_MVC
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+        public static void Main(string[] args) => BuildWebHost(args).Run();
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+                   .UseStartup<Startup>()
+                   .Build();
     }
 }
